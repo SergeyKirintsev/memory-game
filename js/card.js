23d3a01bf2becae97c550b4a645cf712acccf79a
SergeyKirintsev <kserg80@gmail.com>
2021-03-20 15:10:15 +0300

    nojekyll

--- js/card.js
+++ js/card.js
@@ -13,7 +13,7 @@ export default class Card {
   generateCard() {
     this._element = this._getTemplate();
     this._imgEl = this._element.querySelector(".img-card");
-    this._imgEl.src = `../img/${this._link}`;
+    this._imgEl.src = `./img/${this._link}`;
 
     this._inner = this._element.querySelector(".flip-card-inner");
     this._setEventListeners();
